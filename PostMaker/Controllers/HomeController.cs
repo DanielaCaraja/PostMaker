337558ef998adcd06ce8485f70c76520d36dae02
Danielacaraja <dcaraja@mail.ru>
2021-11-11 17:17:28 +0200

    Добавьте файлы проекта.

---
+++ PostMaker/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PostMaker.Models;
+
+
+namespace PostMaker.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreatePost()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreatePost (CreatePostViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View();
+        }
+    }
+    
+
+}
